13e9be5bc1387ee8fd4a2031356b0712a7abbc31
gous123shaikh <shaikhmg29@gmail.com>
2026-03-22 15:51:37 +0530

    Added calculator code

---
+++ calculator.py
@@ -0,0 +1,28 @@
+import math
+
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
+
+    def square_root(self, x):
+        return math.sqrt(x)
+
+calculator = Calculator()
+
+print(calculator.add(10,5))
+print(calculator.subtract(10,5))
+print(calculator.multiply(10,5))
+print(calculator.divide(10,5))
+print(calculator.square_root(25))
